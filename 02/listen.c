6781460297285886a2a4b844c3df19f6935df966
LapTopBot <gerlachbenedikt@gmail.com>
2018-05-16 15:19:24 +0200

    minor changes

--- 02/listen.c
+++ 02/listen.c
@@ -17,37 +17,37 @@ bool is_empty(stud_type** liste) {
 }
 
 void sortier_liste(stud_type** studenten_liste){
-	
+
 }
 
 void sort_vorname(stud_type** studenten_liste) {
-	
+
 }
 
 void sort_nachname(stud_type** studenten_liste) {
-	
+
 }
 
 
-/* Einfuegen eines Elementes    
- * 
+/* Einfuegen eines Elementes
+ *
  * Bekommt einen Zeiger auf einen Zeiger der auf das 1. Element der Liste zeigt
- * Bekommt MatNr, Vorname und Nachname des Studenten der Eingefügt werden soll
+ * Bekommt MatNr, Vorname und Nachname des Studenten der Eingefuegt werden soll
  *
  */
 void enqueue(stud_type** studenten_liste, int matnum, char vorname[20], char nachname[20])
 {
-    /* Deklariere benötigte Variablen */
+    /* Deklariere benoetigte Variablen */
 	stud_type* new_student;
 	stud_type* pcur;
     stud_type* previous;
-    /* Hol Speicher auf dem Heap an für den neuen Listen Eintrag */
+    /* Hol Speicher auf dem Heap an fuer den neuen Listen Eintrag */
 	new_student = malloc(sizeof(stud_type));
-    /* Befüll den Speicher */
+    /* Befuell den Speicher */
 	new_student->matnum = matnum;
 	strcpy(new_student->vorname, vorname);
 	strcpy(new_student->nachname, nachname);
-    /* Füg den neuen Eintrag in die Liste ein */
+    /* Fueg den neuen Eintrag in die Liste ein */
     /* Ist die Liste leer ? */
 	if (is_empty(studenten_liste)){
 		*studenten_liste = new_student;
@@ -64,52 +64,52 @@ void enqueue(stud_type** studenten_liste, int matnum, char vorname[20], char nac
 	}
 }
 
-/* Löschen eines Elementes. 
- * 
- * Bekommt einen Zeiger auf einen Zeiger der auf das 1. Element der Liste zeigt
- * Bekommt die MatNr des Studenten der zu löschen ist
+/* Loeschen eines Elementes.
  *
- * Gibt 0 für einen Fehler zurück
- * Gibt 1 für Erfolg zurück
+ * Bekommt einen Zeiger auf einen Zeiger der auf das 1. Element der Liste zeigt
+ * Bekommt die MatNr des Studenten der zu loeschen ist
+ *
+ * Gibt 0 fuer einen Fehler zurueck
+ * Gibt 1 fuer Erfolg zurueck
  */
 int dequeue(stud_type** studenten_liste, int matnum)
 {
-    /* Deklariere benötigte Variablen -> m*/
+    /* Deklariere benoetigte Variablen -> m*/
     stud_type* pcur;
     stud_type* previous;
-    /* Prüfe Randbedingungen */
+    /* Pruefe Randbedingungen */
 	if(is_empty(studenten_liste)) return 0;
     /* Finde den Studenten */
-    /* Was muss passieren wenn das 1. Element gelöscht wird? */
+    /* Was muss passieren wenn das 1. Element geloescht wird? */
 	pcur = *studenten_liste;
 	if((*studenten_liste)->matnum == matnum){
 		(*studenten_liste) = (*studenten_liste)->next_student;
 		free(pcur);
 		return 1;
 	}
-	
+
 	while(pcur != NULL && pcur->matnum != matnum){
 		previous = pcur;
         pcur = pcur->next_student;
 	}
-	
+
 	if(pcur == NULL) {
         return 0;
     }
-    /* Lösche den Studenten und gibt den Speicher frei */
+    /* Loesche den Studenten und gibt den Speicher frei */
 	if(pcur->matnum == matnum){
 		previous->next_student = pcur->next_student;
 		free(pcur);
 		return 1;
 	}
-    
+
     /* Was ist wenn es nicht in der Liste ist? */
 	else return 0;
     /* ... */
-    
+
 }
 
-/* Auslesen eines Elementes 
+/* Auslesen eines Elementes
  *
  * Bekommt zeiger auf den Listenstart
  * Bekommt matnr des Studenten der ausgelesen werden soll
@@ -127,27 +127,27 @@ int get_student(stud_type* studenten_liste, int matnum, char vorname[20], char n
     }
 
     if ((curr == NULL) || (curr->matnum != matnum)) {
-        /* Rückgabewert: Fehler */
+        /* Rueckgabewert: Fehler */
         return 0;
     } else {
        strncpy(vorname, curr->vorname, 20);
        strncpy(nachname, curr->nachname, 20);
-        /* Rückgabewert: OK */
+        /* Rueckgabewert: OK */
        return 1;
    }
 }
 
 
 /* Test der Listenfunktionen  */
-int main(void)                                     
+int main(void)
 {
     /* Initialisierung der Datenbank */
     stud_type *studenten_liste = NULL;
-    /* platz für vorname */
+    /* platz fuer vorname */
     char vorname[20];
-    /* platz für nachname */
+    /* platz fuer nachname */
     char nachname[20];
-    /* zeiger für iteration */
+    /* zeiger fuer iteration */
     stud_type *curr;
 
     printf(">>> Fuege neuen Studenten in die Liste ein: Eddard Stark [1234] ...\n");
@@ -189,9 +189,9 @@ int main(void)
     } else {
         printf("    Matrikelnummer %4i ist unbekannt\n", 5678);
     }
- 
+
     printf(">>> Loesche die Matrikelnummer 9998 ...\n");
- 
+
     if(dequeue(&studenten_liste, 9998)) {
         printf("    Matrikelnummer %4i geloescht\n", 9998);
     } else {
@@ -213,6 +213,6 @@ int main(void)
         printf("    Matrikelnummer %4i: %s %s\n", curr->matnum, curr->vorname, curr->nachname);
         curr = curr->next_student;
     }
-    
+
     return 0;
 }
